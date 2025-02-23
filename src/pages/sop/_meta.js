3218d912020b3352e58724ca25c4a36e901766c2
Arina Vasiljeva <mistclick.a@gmail.com>
2025-02-23 21:37:07 +0100

    finished the software guides

---
+++ src/pages/sop/_meta.js
@@ -0,0 +1,13 @@
+const contents = {
+  index: {
+    title: "Standard Operating Procedures",
+    theme: {
+      breadcrumb: false,
+      sidebar: false,
+      pagination: false,
+      timestamp: false,
+    },
+  },
+};
+
+export default contents;
