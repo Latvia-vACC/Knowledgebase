90bcfbec4402e0da4ae56f58d63c9b6da1f802dd
Arina Vasiljeva <mistclick.a@gmail.com>
2025-06-29 17:36:10 +0200

    intoduce corrections submitted by mentors

---
+++ src/pages/basics/airspace/_meta.js
@@ -0,0 +1,8 @@
+const components = {
+  "airspace-classes": "Airspace Classes",
+  "airspace-structure": "Airspace Structure",
+  "ats-units": "ATS Units",
+  "barometric-air-pressure": "Barometric Air Pressure",
+};
+
+export default components;
